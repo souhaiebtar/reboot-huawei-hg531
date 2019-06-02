07f1998b766f1e1122cb1a5b0a77a538a39cdf5e
tsouhaieb <tarhounisouhaieb@gmail.com>
2019-06-02 17:35:54 +0100

    work but confirmation

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const puppeteer = require('puppeteer');
 
+function pageClick(page,selector) {
+  page.evaluate( s => document.querySelector(s).click(),selector);
+}
+
 async function timeout(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -7,7 +11,7 @@ async function timeout(ms) {
 puppeteer.launch({headless: false}).then(async browser => {
   const page = await browser.newPage();
   await page.setViewport({ width: 1280, height: 800 })
-  await page.goto('http://192.168.1.1',{ waitUntil : ['load', 'domcontentloaded']});
+  await page.goto('http://192.168.1.1',{waitUntil: 'networkidle0'});
 
     await page.focus('#txt_Username');
     await page.keyboard.type('topadmin');
@@ -22,15 +26,31 @@ puppeteer.launch({headless: false}).then(async browser => {
 
 
   await page.click('#btnLogin');
+  // pageClick(page, '#btnLogin');
+
   await timeout(2000);
+  // open help work
+  // const frame = page.frames().find(frame => frame.name() === 'logofrm');
+  // await frame.evaluate(() => {
+  //   clickHelp();
+  // });
+  const frame = page.frames().find(frame => frame.name() === 'menufrm');
+  await frame.click('#link_User_3');
+  await timeout(2000);
+  await frame.click('#link_User_3_1');
+  await timeout(3000);
+  const contentFrame = page.frames().find(frame => frame.name() === 'contentfrm');
+  const element = await contentFrame.$('[name="btnReboot"]');
+  await element.click();
+  // const response = await page.waitForNavigation({waitUntil:'networkidle2'});
 
-
+  // await page.evaluate(() => clickLogout());
 
   // await page.waitForSelector('#sethelp');
-  // await page.click('#sethelp');
 
-  // await timeout(7000);
-  // await page.screenshot({ path: 'myscreenshot.png', fullPage: true });
+
+  await timeout(4000);
+  await page.screenshot({ path: 'myscreenshot.png', fullPage: true });
 
   await timeout(13000);
   await browser.close();
