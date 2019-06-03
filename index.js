8769aa5e17a80e1f648e897305e7ff674e6c2b9b
tsouhaieb <tarhounisouhaieb@gmail.com>
2019-06-03 03:37:27 +0100

    it work to reboot

--- index.js
+++ index.js
@@ -8,50 +8,36 @@ async function timeout(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-puppeteer.launch({headless: false}).then(async browser => {
+puppeteer.launch({headless: true}).then(async browser => {
   const page = await browser.newPage();
+  page.on("dialog", async dialog => {
+    console.log("the dialog message is", dialog.message());
+    console.log("the dialog type is", dialog.type());
+    await dialog.accept();
+    });
   await page.setViewport({ width: 1280, height: 800 })
   await page.goto('http://192.168.1.1',{waitUntil: 'networkidle0'});
 
     await page.focus('#txt_Username');
-    await page.keyboard.type('topadmin');
+    await page.keyboard.type('user');
 
     await page.focus('#txt_Password');
-    await page.keyboard.type('topadmin');
-  
-    // const loginInput=await page.$('#txt_Username');
-    // loginInput.value='topadmin';
-    // const passwordInput=await page.$('#txt_Password');
-    // passwordInput.value='topadmin';
+    await page.keyboard.type('user1234');
 
 
   await page.click('#btnLogin');
-  // pageClick(page, '#btnLogin');
 
   await timeout(2000);
-  // open help work
-  // const frame = page.frames().find(frame => frame.name() === 'logofrm');
-  // await frame.evaluate(() => {
-  //   clickHelp();
-  // });
   const frame = page.frames().find(frame => frame.name() === 'menufrm');
   await frame.click('#link_User_3');
-  await timeout(2000);
+  await timeout(1000);
   await frame.click('#link_User_3_1');
-  await timeout(3000);
+  await timeout(1000);
   const contentFrame = page.frames().find(frame => frame.name() === 'contentfrm');
   const element = await contentFrame.$('[name="btnReboot"]');
   await element.click();
-  // const response = await page.waitForNavigation({waitUntil:'networkidle2'});
-
-  // await page.evaluate(() => clickLogout());
-
-  // await page.waitForSelector('#sethelp');
 
 
-  await timeout(4000);
-  await page.screenshot({ path: 'myscreenshot.png', fullPage: true });
-
-  await timeout(13000);
   await browser.close();
+  process.exit(1);
 });
