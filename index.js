40d2e0c59fe5a802a36b665a4289f8c61a87ce16
tsouhaieb <tarhounisouhaieb@gmail.com>
2019-06-30 19:18:45 +0100

    working properly

--- index.js
+++ index.js
@@ -23,11 +23,22 @@ puppeteer.launch({headless: true}).then(async browser => {
 
     await page.focus('#txt_Password');
     await page.keyboard.type('user1234');
+  
+    // const loginInput=await page.$('#txt_Username');
+    // loginInput.value='topadmin';
+    // const passwordInput=await page.$('#txt_Password');
+    // passwordInput.value='topadmin';
 
 
   await page.click('#btnLogin');
+  // pageClick(page, '#btnLogin');
 
   await timeout(2000);
+  // open help work
+  // const frame = page.frames().find(frame => frame.name() === 'logofrm');
+  // await frame.evaluate(() => {
+  //   clickHelp();
+  // });
   const frame = page.frames().find(frame => frame.name() === 'menufrm');
   await frame.click('#link_User_3');
   await timeout(1000);
@@ -36,6 +47,11 @@ puppeteer.launch({headless: true}).then(async browser => {
   const contentFrame = page.frames().find(frame => frame.name() === 'contentfrm');
   const element = await contentFrame.$('[name="btnReboot"]');
   await element.click();
+  // const response = await page.waitForNavigation({waitUntil:'networkidle2'});
+
+  // await page.evaluate(() => clickLogout());
+
+  // await page.waitForSelector('#sethelp');
 
 
   await browser.close();
